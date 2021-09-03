cd9d20377586d7fcc45f9e610c40d73194cec1cc
vongovantien <vongovantien@gmail.com>
2021-09-03 21:31:10 +0700

    project update

--- script/script.js
+++ script/script.js
@@ -1,17 +1,28 @@
 
-$(document).ready(function(){
-    // $("button.btn").click(function() {
-    //   $("div#map").slideToggle(2000);
-    // });
-    // $("div.vehicle-item-col").click(function () {
-    //   $("div.packages-item__price > span").text() * 2;
-    // });
-  $(window).scroll(function(event){
-    var pos_body = $("html body").scrollTop();
-    var pos_img2 = $("div:nth-child(2)").offset().top;
-
-    if(pos_body>pos_img2-200){
-      $("div:nth-child(2)").addClass("animate__animated animate__fadeInLeft");
+  // $("button.btn").click(function () {
+  //   $("div#map").slideToggle(2000);
+  // });
+  // $("div.vehicle-item-col").click(function () {
+  //   $("div.packages-item__price > span").text() * 2;
+  // });
+  $(window).scroll(function () {
+    var pos_body = $(window).scrollTop();
+    if (pos_body >= 100) {
+      $(".header").addClass("fixed-top");
+    }
+    else
+    {
+      $(".header").removeClass("fixed-top");
     }
   });
-});
+// Thêm icon bar vào màn hình
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
